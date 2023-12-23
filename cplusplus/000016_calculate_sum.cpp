22570df0405dd332c27edbfe5cbf448309782d65
tuanloc-vocong <vocong.tuanloc@gmail.com>
2023-12-23 19:32:20 +0700

    calculate sum

---
+++ cplusplus/000016_calculate_sum.cpp
@@ -0,0 +1,40 @@
+// Title: Calculate Sum
+// Difficulty: Easy
+// Tags:
+// Author: tuanloc-vocong
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// Topic:
+
+/**
+ * Viết chương trình đọc vào số nguyên dương n và tính tổng các số nguyên từ 1 đến n, tức là (1+2+...+n).
+ * 
+ * Input:
+ * Dòng đầu tiên chứa số nguyên dương n (n≤100).
+ * 
+ * Output:
+ * In tổng S.
+ *  
+ * Example 1:
+ * Input:
+ * 5
+ * 
+ * Output:
+ * 15
+ **/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, sum = 0;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        sum += i;
+    }
+
+    cout << sum;
+    return 0;
+}
