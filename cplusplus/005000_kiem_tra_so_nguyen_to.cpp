1f107af29f0b26b5a651f351b35f017ecc91885b
vocongtuanloc <vocong.tuanloc@gmail.com>
2024-05-30 17:11:39 +0700

    005002 sang so nguyen to tren doan

---
+++ cplusplus/005000_kiem_tra_so_nguyen_to.cpp
@@ -0,0 +1,57 @@
+// Title: Kiểm Tra Số Nguyên Tố
+// Source: 28tech
+// Difficulty: Easy
+// Tags:
+// Author: tuanloc-vocong
+// Time Complexity: O(logn)
+// Space Complexity:
+// Topic: Number Theory
+
+/**
+ * Input:
+ * Số nguyên tố n (0≤n≤10^9).
+ *
+ * Output:
+ * In YES nếu n là số nguyên tố, ngược lại in NO.
+ *
+ * Example 1:
+ * Input:
+ * 4
+ *
+ * Output:
+ * NO
+ *
+ * Example 2:
+ * Input:
+ * 13
+ *
+ * Output:
+ * YES
+ **/
+
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+int prime(int n)
+{
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            return 0;
+        }
+    }
+    return n > 1;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if (prime(n))
+        printf("YES");
+    else
+        printf("NO");
+}
---
+++ cplusplus/005002_sang_so_nguyen_to_tren_doan.cpp
@@ -0,0 +1,38 @@
+// Title: Sàng Số Nguyên Tố Trên Đoạn
+// Source: 28tech
+// Difficulty: Easy
+// Tags:
+// Author: tuanloc-vocong
+// Time Complexity: O(nlog(logn))
+// Space Complexity:
+// Topic: Number Theory
+
+/**
+ * Input:
+ * 2 số nguyên không âm a, b(0≤a≤b≤10^9,b-a≤10^5).
+ *
+ * Output:
+ * In ra các số nguyên tố trong đoạn từ a tới b (Chú ý lấy cả 2 cận).
+ *
+ * Example 1:
+ * Input:
+ * 4 20
+ *
+ * Output:
+ * 5 7 11 13 17 19
+ *
+ * Example 2:
+ * Input:
+ * 1 5
+ *
+ * Output:
+ * 2 3 5
+ **/
+
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+}
